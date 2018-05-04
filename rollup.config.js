bb5544668e9cbbf0bfe338a1c91c3bef0f374028
Walker Leite <walker@incuca.com.br>
2018-05-04 16:02:39 -0300

    fix node module error

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,9 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json';
 import pkg from './package.json';
+import fixMktmpdir from './.rollup/fix-mktmpdir';
 
 export default {
     input: 'index.js',
@@ -11,8 +13,12 @@ export default {
         format: 'cjs'
     },
     plugins: [
-        resolve(),
+        fixMktmpdir,
+        resolve({
+          preferBuiltins: true,
+        }),
         commonjs(),
+        json(),
         babel({
             presets: [
                 [
