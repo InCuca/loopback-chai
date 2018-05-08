205ca55a06ee6a3f11bbc92ed5c0461b5da6e63e
Walker Leite <walker@incuca.com.br>
2018-05-08 20:35:38 -0300

    add all haveMany tests

--- src/methods.js
+++ src/methods.js
@@ -1,10 +1,22 @@
 import { expect } from 'chai';
 
-export function haveMany(relationship) {
+export function haveMany(relationship, model, foreignKey) {
   expect(this._obj).to.haveOwnProperty(relationship);
   expect(this._obj[relationship]).to.be.an('object');
 
   const relObj = this._obj[relationship];
   expect(relObj).to.haveOwnProperty('type');
   expect(relObj.type).to.equal('hasMany');
+
+  expect(relObj).to.haveOwnProperty('model');
+  expect(relObj.model).to.not.empty;
+  if (model) {
+    expect(relObj.model).to.equal(model);
+  }
+
+  expect(relObj).to.haveOwnProperty('foreignKey');
+  expect(relObj.foreignKey).to.not.empty;
+  if (foreignKey) {
+    expect(relObj.foreignKey).to.equal(foreignKey);
+  }
 }
--- src/methods.spec.js
+++ src/methods.spec.js
@@ -2,26 +2,63 @@ import chai from 'chai';
 import loopbackChai from './index';
 
 describe('haveMany assertion', () => {
+  const perfectRelMock = {
+    balls: {
+      type: 'hasMany',
+      model: 'Ball',
+      foreignKey: 'ballId',
+    },
+  };
+
   chai.use(loopbackChai);
 
-  function hasMany(obj, ...args) {
+  // Wrapper for haveMany assertion
+  function hasMany(subject, ...args) {
     return () => chai
-      .expect(obj)
+      .expect(subject)
       .to
       .haveMany(...args);
   }
 
-  it('do assert haveMany relationship', () => {
-    expect(hasMany({
-      balls: {
-        type: 'hasMany',
-        model: 'Ball',
-        foreignKey: 'modelId',
-      },
-    }, 'balls')).not.toThrow();
+  // Check if haveMany throws when checking attribute
+  function itThrows(name) {
+    const errMock = {
+      ...perfectRelMock,
+      ...{ balls: { ...perfectRelMock.balls } },
+    };
+    const hasManyFn = hasMany(
+      errMock,
+      'balls',
+      perfectRelMock.balls.model,
+      perfectRelMock.balls.foreignKey,
+    );
+
+    delete errMock.balls[name];
+    expect(hasManyFn).toThrow();
+
+    errMock.balls[name] = undefined;
+    expect(hasManyFn).toThrow();
+
+    errMock.balls[name] = null;
+    expect(hasManyFn).toThrow();
+
+    errMock.balls[name] = 'INVALID_VALUE';
+    expect(hasManyFn).toThrow();
+  }
+
+  it('throws if wrong relationship type', () => {
+    itThrows('type');
   });
 
-  it('do not assert haveMany if missing relationship name', () => {
+  it('throws if wrong relationship model', () => {
+    itThrows('model');
+  });
+
+  it('throws if wrong relationship foreignKey', () => {
+    itThrows('foreignKey');
+  });
+
+  it('throws if missing relationship name', () => {
     expect(hasMany({})).toThrow();
     expect(hasMany({ noop: {} }, 'balls')).toThrow();
     expect(hasMany({ balls: undefined }, 'balls')).toThrow();
@@ -29,10 +66,11 @@ describe('haveMany assertion', () => {
     expect(hasMany({ balls: '' }, 'balls')).toThrow();
   });
 
-  it('do not assert haveMany if missing relationship type', () => {
-    expect(hasMany({ balls: {} }, 'balls')).toThrow();
-    expect(hasMany({ balls: { type: undefined } }, 'balls')).toThrow();
-    expect(hasMany({ balls: { type: null } }, 'balls')).toThrow();
-    expect(hasMany({ balls: { type: 'belongsTo' } }, 'balls')).toThrow();
+  it('assert haveMany relationship', () => {
+    const subj = perfectRelMock;
+    const { model, foreignKey } = perfectRelMock.balls;
+    expect(hasMany(subj, 'balls')).not.toThrow();
+    expect(hasMany(subj, 'balls', model)).not.toThrow();
+    expect(hasMany(subj, 'balls', model, foreignKey)).not.toThrow();
   });
 });
