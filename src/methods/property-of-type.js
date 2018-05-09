dd0c6b91ecfebb9ab9abdbb1416e9135dc94667a
Walker Leite <walker@incuca.com.br>
2018-05-09 12:26:23 -0300

    refactor(src): remove all codes

--- src/methods/property-of-type.js
+++ src/methods/property-of-type.js
@@ -1,17 +1,17 @@
-// import { expect } from 'chai';
+// // import { expect } from 'chai';
 
-export function propertyOfType() {
-  // const subj = this._obj;
-  // expect(subj.properties).to.haveOwnProperty(property);
-  // const prop = subj.properties[property];
-  // expect(prop).to.haveOwnProperty('type');
-  // expect(prop.type).to.oneOf([
-  //   'any',
-  //   'array',
-  //   'boolean',
-  //   'buffer',
-  //   'date',
-  //   'GeoPoint',
-  //   ''
-  // ])
-}
+// export function propertyOfType() {
+//   // const subj = this._obj;
+//   // expect(subj.properties).to.haveOwnProperty(property);
+//   // const prop = subj.properties[property];
+//   // expect(prop).to.haveOwnProperty('type');
+//   // expect(prop.type).to.oneOf([
+//   //   'any',
+//   //   'array',
+//   //   'boolean',
+//   //   'buffer',
+//   //   'date',
+//   //   'GeoPoint',
+//   //   ''
+//   // ])
+// }
