fd20bee6a25fc541c40b2509587510ee37647632
Walker Leite <walker@incuca.com.br>
2018-05-09 17:31:28 -0300

    feat(test-utils): add createMockedModel tests

--- test-utils/create-mocked-model.js
+++ test-utils/create-mocked-model.js
@@ -1,6 +1,18 @@
 import { ModelBuilder } from 'loopback-datasource-juggler';
 
-export default function (name) {
+export default function (name, schema = {}) {
+  const defaultSchema = {
+    name,
+    properties: {},
+  };
+  if (name) defaultSchema.plural = `${name}s`;
+
   const builder = new ModelBuilder();
-  return builder.buildModels({ name });
+  const models = builder.buildModels({
+    ...defaultSchema,
+    ...schema,
+  });
+  const keys = Object.keys(models);
+
+  return models[keys[0]];
 }
--- test-utils/create-mocked-model.spec.js
+++ test-utils/create-mocked-model.spec.js
@@ -4,4 +4,11 @@ describe('createMockedModel test util', () => {
   it('throws without model name', () => {
     expect(() => createMockedModel()).toThrow();
   });
+
+  it('creates an ModelClass', () => {
+    const stub = {};
+    const Foo = createMockedModel('Foo', stub);
+    expect(Foo.prototype).toEqual(stub);
+    expect(Foo.definition.name).toEqual('Foo');
+  });
 });
