75881cb5bd4ffd335ac0ffdad5b13bb07365ee77
Walker <walker@praiseweb.com.br>
2018-05-10 12:30:32 -0300

    fix(property-of-type): remove useless comments

--- src/methods/property-of-type.spec.js
+++ src/methods/property-of-type.spec.js
@@ -2,42 +2,6 @@ import chai from 'chai';
 import { createMockedModel } from '../../test-utils';
 import loopbackChai from '../index';
 
-// import loopbackChai from '../index';
-
-// describe('propertyOfType assertion', () => {
-// const perfecMock = {
-//   properties: {
-//     foo: {
-//       type: 'string',
-//     },
-//   },
-// };
-
-// chai.use(loopbackChai);
-
-// function propertyOfType(subject, ...args) {
-//   return () => chai
-//     .expect(subject)
-//     .to
-//     .propertyOfType(...args);
-// }
-
-// it('throws if missing property name', () => {
-//   const subj = {
-//     properties: { foo: { type: 'INVALID' } },
-//   };
-
-//   expect(propertyOfType(subj)).toThrow();
-// });
-
-// it('throws if wrong type', () => {
-//   const subj = {
-//     properties: { foo: { type: 'INVALID' } },
-//   };
-//   expect(propertyOfType(subj, 'foo')).toThrow();
-// });
-// });
-
 describe('propertyOfType assertion', () => {
   function propertyOfType(settings, ...args) {
     return () => {
